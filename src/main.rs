29387060697d8f8d986f4198b213ffc0fae9c686
essamgouda97 <essamgouda97@hotmail.com>
2023-02-25 19:30:15 -0700

    encrypt done

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,12 @@ use chacha20poly1305::{
 };
 use serde::{Serialize, Deserialize};
 use serde_json;
+use cocoon::*;
+use pdfium_render::prelude::*;
 
 // Constansts
+const BIN_PATH: &str = ".\\bin\\";
+const FILES_PATH: &str = ".\\Files";
 const USB_ID_FILE: &str = "./System Volume Information/IndexerVolumeGuid";
 const KEY: [u8; 32] = [201, 246, 255, 188, 110, 230, 
                     94, 198, 192, 77, 164, 20, 5, 122, 
@@ -29,6 +33,7 @@ const NONCE: [u8; 24] =  [213, 85, 53, 54, 40, 196,
                         219, 239, 120, 62, 63, 205, 
                         43, 146, 64, 252, 128, 242, 
                         103, 225, 59, 7, 43, 130];
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct MetaData {
@@ -70,11 +75,56 @@ fn get_usb_id() -> String{
    return content;
 }
 
+fn export_pdf_to_jpegs(path: &str, password: Option<&str>) -> Result<(), PdfiumError> {
+    // Renders each page in the PDF file at the given path to a separate JPEG file.
+
+    // Bind to a Pdfium library in the same directory as our Rust executable;
+    // failing that, fall back to using a Pdfium library provided by the operating system.
+    println!("Creating pdf obj");
+    let pdfium = Pdfium::new(
+        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(BIN_PATH))
+        .or_else(|_| Pdfium::bind_to_system_library())?,
+        );
+
+    // Load the document from the given path...
+    println!("Loading pdf document from {}", path);
+    let document = pdfium.load_pdf_from_file(path, password).expect("Can't open pdf file");
+
+    // ... set rendering options that will be applied to all pages...
+    println!("Rendering pdf");
+    let render_config = PdfRenderConfig::new()
+        .set_target_width(2000)
+        .set_maximum_height(2000)
+        .rotate_if_landscape(PdfBitmapRotation::Degrees90, true);
+
+    // ... then render each page to a bitmap image, saving each image to a JPEG file.
+    let cipher = XChaCha20Poly1305::new(&KEY.into());
+    for (index, page) in document.pages().iter().enumerate() {
+        let temp = page.render_with_config(&render_config)
+                    .unwrap()
+                    .as_image()
+                    .as_rgba8()
+                    .unwrap()
+                    .clone();
+
+        
+        
+        println!("Encrypting {}", index);
+        let encrypted_data = cipher
+            .encrypt(&NONCE.into(), temp.into_raw().as_ref())
+            .unwrap(); 
+        fs::write(format!("{}_{}.bin", path, index), encrypted_data);
+
+    }
+
+    Ok(())
+}
+
 fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
    
-    let paths = fs::read_dir(".\\Files")?;
+    let paths = fs::read_dir(FILES_PATH)?;
     let cipher = XChaCha20Poly1305::new(&KEY.into());
-
+    
     // Main encrypt loop
     let mut db = DB::new(usb_id); 
 
@@ -87,11 +137,8 @@ fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
         if ! p.ends_with(".pdf"){continue;};
        
         let file_data = fs::read(&p)?;
-        let encrypted_file = cipher
-            .encrypt(&NONCE.into(), file_data.as_ref())
-            .map_err(|err| anyhow!("Encrypting file: {}", err))?;
-        let out_file = p.replace(".pdf", ".bin");
-        fs::write(out_file, encrypted_file)?; // save encrypted file
+        println!("Converting {} to jpegs", filename); 
+        let pdf_done = export_pdf_to_jpegs(&filename, None).unwrap();
         fs::remove_file(p)?; // remove pdf
     
         // Ask user for number of times for file
@@ -109,12 +156,12 @@ fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
     
 
     // Save db.json
-    let db_file = File::create("db.json").expect("Failed to create db.json");
+    let mut db_file = File::create("db.json").expect("Failed to create db.json");
     let db = serde_json::to_string(&db).unwrap();
     let encrypted_db = cipher
         .encrypt(&NONCE.into(), db.as_ref())
         .map_err(|err| anyhow!("Ecrypting db: {}", err))?;
-    fs::write("db.json", encrypted_db)?;
+    fs::write("db.json", encrypted_db)?;  
 
     }
 
@@ -128,17 +175,16 @@ fn decrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
     let cipher = XChaCha20Poly1305::new(&KEY.into());
 
     // Read db.json
-    let db_data = fs::read("db.json").expect("db.json not found, make sure to run encrypt mode");
-
-    let db_file = cipher
-        .decrypt(&NONCE.into(), db_data.as_ref())
+    let db_data_encrypted = fs::read("db.json").expect("db.json not found, make sure to run encrypt mode");
+    let db_data_decrypted = cipher
+        .decrypt(&NONCE.into(), db_data_encrypted.as_ref())
         .map_err(|err| anyhow!("Decrypting db: {}", err))?;
     
-    let decoded_db_file = match str::from_utf8(&db_file) {
+    let db_data_decoded = match str::from_utf8(&db_data_decrypted) {
         Ok(v) => v,
         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
     };
-    let db_data: DB = serde_json::from_str(decoded_db_file).unwrap();
+    let db_data: DB = serde_json::from_str(db_data_decoded).unwrap();
 
     // Confirm usb_id
     let current_usb_id = get_usb_id();
@@ -146,6 +192,21 @@ fn decrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
         println!("pdf-secure can only run on the same usb device. {} != {}", db_data.usb_id, current_usb_id);
         process::exit(0);
     }
+
+    // Ask user which file to view
+
+
+    // View file
+    let temp_filename = "test.bin"; // TODO replace by file picker
+   
+    let full_path = format!("{}\\{}", FILES_PATH, temp_filename);
+    let pdf_data_encrypted= fs::read(full_path).expect("Couldn't read pdf file");
+    let pdf_data_decrypted: Vec<u8> = cipher
+        .decrypt(&NONCE.into(), pdf_data_encrypted.as_ref())
+        .map_err(|err| anyhow!("Decrypting pdf: {}", err))?;
+    
+    
+    fs::write("test.pdf", pdf_data_decrypted)?;
 
     Ok(())
 }
