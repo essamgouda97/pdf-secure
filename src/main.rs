a975f8838681ffd294807f1bfc7e68106a516520
essamgouda97 <essamgouda97@hotmail.com>
2023-03-05 22:47:22 -0700

    decrypt done

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use chacha20poly1305::{
 };
 use serde::{Serialize, Deserialize};
 use serde_json;
-use cocoon::*;
 use pdfium_render::prelude::*;
+use show_image::{create_window, event};
 
 // Constansts
 const BIN_PATH: &str = ".\\bin\\";
@@ -39,6 +39,7 @@ const NONCE: [u8; 24] =  [213, 85, 53, 54, 40, 196,
 struct MetaData {
     file_open_count: i8, // number of times file is opened
     max_files_open_count: i8, // maximum number of times a file is allowed to be opened
+    renders_config: HashMap<usize, (u32, u32)>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -75,7 +76,7 @@ fn get_usb_id() -> String{
    return content;
 }
 
-fn export_pdf_to_jpegs(path: &str, password: Option<&str>) -> Result<(), PdfiumError> {
+fn export_pdf_to_jpegs(path: &str, password: Option<&str>) -> Result<HashMap<usize, (u32, u32)>, PdfiumError> {
     // Renders each page in the PDF file at the given path to a separate JPEG file.
 
     // Bind to a Pdfium library in the same directory as our Rust executable;
@@ -96,28 +97,32 @@ fn export_pdf_to_jpegs(path: &str, password: Option<&str>) -> Result<(), PdfiumE
         .set_target_width(2000)
         .set_maximum_height(2000)
         .rotate_if_landscape(PdfBitmapRotation::Degrees90, true);
+    
+    let cipher = XChaCha20Poly1305::new(&KEY.into());
 
     // ... then render each page to a bitmap image, saving each image to a JPEG file.
-    let cipher = XChaCha20Poly1305::new(&KEY.into());
+    let mut render_config_data = HashMap::new();
     for (index, page) in document.pages().iter().enumerate() {
-        let temp = page.render_with_config(&render_config)
-                    .unwrap()
-                    .as_image()
-                    .as_rgba8()
-                    .unwrap()
-                    .clone();
+        let temp = page
+            .render_with_config(&render_config)
+            .unwrap()
+            .as_image()
+            .as_rgba8()
+            .unwrap()
+            .clone();
 
-        
-        
+        let (x, y) = temp.dimensions();
+        render_config_data.insert(index, (x,y));
+        println!("{}:{}", x, y);
         println!("Encrypting {}", index);
         let encrypted_data = cipher
             .encrypt(&NONCE.into(), temp.into_raw().as_ref())
-            .unwrap(); 
+            .unwrap();
         fs::write(format!("{}_{}.bin", path, index), encrypted_data);
-
+     
     }
 
-    Ok(())
+    Ok(render_config_data)
 }
 
 fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
@@ -138,7 +143,7 @@ fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
        
         let file_data = fs::read(&p)?;
         println!("Converting {} to jpegs", filename); 
-        let pdf_done = export_pdf_to_jpegs(&filename, None).unwrap();
+        let renders_config = export_pdf_to_jpegs(&filename, None).unwrap();
         fs::remove_file(p)?; // remove pdf
     
         // Ask user for number of times for file
@@ -151,6 +156,7 @@ fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
         let db_data = MetaData{
             file_open_count: 1,
             max_files_open_count: max_number_open,
+            renders_config: renders_config,
         };
         db.join(&filename, db_data);         
     
@@ -167,7 +173,9 @@ fn encrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
 
     Ok(())
 }
-
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
 
 fn decrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
 
@@ -194,22 +202,73 @@ fn decrypt_mode(usb_id: &String) -> Result<(), anyhow::Error>{
     }
 
     // Ask user which file to view
-
+    
 
     // View file
-    let temp_filename = "test.bin"; // TODO replace by file picker
+    let mut index: usize = 0;
+    let temp_filename: &str = ".\\Files\\test.pdf"; // TODO replace by file picker
+    let file_metadata: &MetaData = db_data.data.get(temp_filename).unwrap();
    
-    let full_path = format!("{}\\{}", FILES_PATH, temp_filename);
-    let pdf_data_encrypted= fs::read(full_path).expect("Couldn't read pdf file");
-    let pdf_data_decrypted: Vec<u8> = cipher
-        .decrypt(&NONCE.into(), pdf_data_encrypted.as_ref())
-        .map_err(|err| anyhow!("Decrypting pdf: {}", err))?;
-    
-    
-    fs::write("test.pdf", pdf_data_decrypted)?;
+    let max_pages = file_metadata.renders_config.len();
+    let dynamic_image = read_image(temp_filename, index, file_metadata).unwrap();
+            
+    // view image
+    let window = create_window("image", Default::default()).expect("Failed to create window");
+    window.set_image("image-001", dynamic_image).expect("Failed to set image");
+    // If the user closes the window, the channel is closed and the loop also exits.
+    for event in window.event_channel()? {
+    if let event::WindowEvent::KeyboardInput(event) = event {
+        if event.input.key_code == Some(event::VirtualKeyCode::Escape) && event.input.state.is_pressed() {
+            break;
+        }
+        if event.input.key_code == Some(event::VirtualKeyCode::Right) && event.input.state.is_pressed() && index+1 < max_pages{
+            index+=1;
+            let dynamic_image = read_image(temp_filename, index, file_metadata).unwrap();
+            window.set_image("image-001", dynamic_image).expect("Failed to set image");
+        }
+        if event.input.key_code == Some(event::VirtualKeyCode::Left) && event.input.state.is_pressed() && index > 0{
+            index-=1;
+            let dynamic_image = read_image(temp_filename, index, file_metadata).unwrap();
+            window.set_image("image-001", dynamic_image).expect("Failed to set image");
+        }
+    }
+}
 
     Ok(())
 }
+
+fn read_image(filename: &str, index: usize, file_metadata: &MetaData) -> Result<image::DynamicImage, anyhow::Error>{
+    
+
+    let cipher = XChaCha20Poly1305::new(&KEY.into());
+
+    let data_encrypted= fs::read(format!("{}_{}.bin", filename, index)).expect("Couldn't read pdf file");
+    let rgba_data: Vec<u8> = cipher
+        .decrypt(&NONCE.into(), data_encrypted.as_ref())
+        .map_err(|err| anyhow!("Decrypting pdf: {}", err))?;
+   
+
+    let (x,y) = file_metadata.renders_config.get(&index).unwrap();
+    let image_buffer: image::RgbImage = image::RgbImage::new(*x, *y);
+    let mut pixel_data  = image_buffer.into_raw();
+
+    for i in 0..rgba_data.len() / 4 {
+        let r = rgba_data[i * 4];
+        let g = rgba_data[i * 4 + 1];
+        let b = rgba_data[i * 4 + 2];
+        pixel_data[i * 3] = r;
+        pixel_data[i * 3 + 1] = g;
+        pixel_data[i * 3 + 2] = b;
+    }
+
+
+    let image_buffer =  image::RgbImage::from_raw(*x, *y, pixel_data).unwrap();
+    let dynamic_image = image::DynamicImage::ImageRgb8(image_buffer);
+    
+    Ok(dynamic_image)
+}
+
+#[show_image::main]
 fn main() {
     println!("Welcome to pdf-secure, a tool to secure your pdf files");
     
